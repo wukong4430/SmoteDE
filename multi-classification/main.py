062d3d3840421787cbc14205296f268e32d3e2f9
Kicc <wukong4430@gmail.com>
2019-01-18 13:00:38 +0800

    复制Processing 创建main

---
+++ multi-classification/main.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 kicc <wukong4430@gmail.com>
+#           Create time: 2019-01-18 12:45
+#
+# Distributed under terms of the MIT license.
+
+"""
+获取csv数据，调用ssmote合成数据
+"""
+from Processing import Processing, convert2numpy
+
+
+
+
+
+def main():
+
+    print('o')
+
+if __name__ == '__main__':
+    main()
