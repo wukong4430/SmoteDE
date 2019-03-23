c49e305560936c03917c11c830ac08347e0fee33
Kicc <wukong4430@gmail.com>
2019-03-23 17:40:44 +0800

    统计pre and post, 另一个csv

--- defectStatistics/Processing.py
+++ defectStatistics/Processing.py
@@ -23,8 +23,8 @@ class Processing(object):
 
     def __init__(self):
         """定义数据的文件夹路径 """
-        self.dataPath= '../datasetcsv'
-        # self.dataPath = '../tinydataset'
+        # self.dataPath= '../../datasetcsv'
+        self.dataPath = '../../pre-post-csv'
         
     def dataload(self):
         """get data from csv
@@ -40,6 +40,7 @@ class Processing(object):
         for root, dirs, files in os.walk(folderPath):
 
             for file in files:
+                print(f'file = {file}')
                 filePath = os.path.join(root, file)
                 dataset = pd.read_csv(filePath)
                 yield dataset, file
@@ -54,13 +55,43 @@ def convert2numpy(dataset):
     """
     dataset = dataset.iloc[:, 3:] # eliminate the first 3 columns.
     dataset = np.array(dataset)
+    print(f'shape of dataset = {dataset.shape}')
+    
     columns = dataset.shape[1]
 
     dataX, datay = dataset[:, 0:columns-1], dataset[:, -1]
     return dataX, datay
         
 
+def convert2numpy_twoY(dataset):
+    """pandas 2 numpy.array
+       对应于3月12日的任务,
+       pre是一个y1, 位于第三列
+       post是一个y2, 位于第四列
+       前两列信息没用
+
+    :dataset: dataset from Processing().dataload()
+    :returns: numpy.array
+
+    """
+    dataset = dataset.iloc[:, 2:] # eliminate the first 2 columns.
+    dataset = np.array(dataset)
+    print(f'shape of dataset = {dataset.shape}')
+    columns = dataset.shape[1]
+
+    dataX, pre, post = dataset[:, 2:columns], dataset[:, 0], dataset[:, 1]
+    print(f'pre = {pre}')
+    print(f'post = {post}')
+    return dataX, pre, post
 
 
 if __name__ == '__main__':
-    main()
+    for dataset, filename in Processing().dataload():
+        print(f'filename = {filename}')
+
+        X, y_pre, y_post = convert2numpy_twoY(dataset)
+        print(X.shape)
+        print(y_pre.shape)
+        print(y_post.shape)
+        # break
+
--- defectStatistics/statistics.py
+++ defectStatistics/statistics.py
@@ -21,11 +21,12 @@ Avg is the average value of defects of all defective-prone modules in the releas
 Avg=#Defect/(#Instance * %Defects)
 """
 import os
-from Processing import Processing, convert2numpy
+from Processing import Processing, convert2numpy, convert2numpy_twoY
 import fire
 import pandas as pd
 import numpy as np
 import csv
+
 
 class Statistics(object):
 
@@ -33,6 +34,7 @@ class Statistics(object):
 
     def __init__(self, data):
         """initial."""
+        # 输入的data 是 y
         self.data = data
 
     def numberInstance(self):
@@ -106,28 +108,47 @@ def fiveElements():
     all_maxs = []
     all_avgs = []
     for dataset, filename in Processing().dataload():
-        data = convert2numpy(dataset)
-        statics = Statistics(data[1])
-        # #Instance
-        # print(filename)
-        number_instance = statics.numberInstance()
-        number_defect = statics.numberDefect()
-        percent_defect = statics.percentDefect()
-        max_bug = statics.max_()
-        avg_bug = statics.avg_()
-        # print('\n')
-        # print('#Instance :{0}'.format(number_instance))
-        # print('#Defects :{0}'.format(number_defect))
-        # print('%Defects :{:.1f}'.format(percent_defect))
-        # print('max_bug :{0}'.format(max_bug))
-        # print('avg_bug :{:.2f}'.format(avg_bug))
+        # data = convert2numpy(dataset)
+        # 对应第一版csv
+        # statics = Statistics(data[1])
 
-        all_filenames.append(filename)
-        all_instances.append(number_instance)
-        all_defects.append(number_defect)
-        all_percent_defects.append(percent_defect)
-        all_maxs.append(max_bug)
-        all_avgs.append(avg_bug)
+        data = convert2numpy_twoY(dataset)
+        # 对应第二版的csv文件,有pre和post两个y
+        statics_loop = []
+        statics_pre = Statistics(data[1])
+        statics_loop.append(statics_pre)
+
+        statics_post = Statistics(data[2])
+        statics_loop.append(statics_post)
+
+        # #Instance
+        idx = 0
+        for statics in statics_loop:
+            # print(filename)
+            number_instance = statics.numberInstance()
+            number_defect = statics.numberDefect()
+            percent_defect = statics.percentDefect()
+            max_bug = statics.max_()
+            avg_bug = statics.avg_()
+            # print('\n')
+            # print('#Instance :{0}'.format(number_instance))
+            # print('#Defects :{0}'.format(number_defect))
+            # print('%Defects :{:.1f}'.format(percent_defect))
+            # print('max_bug :{0}'.format(max_bug))
+            # print('avg_bug :{:.2f}'.format(avg_bug))
+
+            if idx == 0:
+                all_filenames.append(filename + '-pre')
+            else:
+                all_filenames.append(filename + '-post')
+
+            all_instances.append(number_instance)
+            all_defects.append(number_defect)
+            all_percent_defects.append(percent_defect)
+            all_maxs.append(max_bug)
+            all_avgs.append(avg_bug)
+
+            idx += 1
 
     print(all_filenames)
     data_ = {'Release': all_filenames,
@@ -137,48 +158,77 @@ def fiveElements():
              'max_bug': all_maxs,
              'avg_bug': all_avgs}
 
-    save_(r'fiveElements.csv', filename, **data_)
+    save_(r'fiveElements2.csv', filename, **data_)
     pass
 
 
-def proportion_(dataset, filename):
+def proportion_(dataset, filename, mode=1):
     """ 统计每个release中各个数量模型的比例
+    如果mode=1, 是统计比例, 全部加起来是1.
+    如果mode=2, 是统计个数, 全部加起来是所有的缺陷数
     :returns: 写入文件
 
     """
-    data = convert2numpy(dataset)
-    statics = Statistics(data[1])
-    max_bug = statics.max_()
-    number_instance = statics.numberInstance()
-    unique, counts = np.unique(data[1], return_counts=True)
-    counter = dict(zip(unique, counts))
-    print(counter)
-    res = dict()
-    res['Release'] = filename
-    for i in range(max_bug + 1):
-        if i>=9:
-            break
-        if i in counter:
-            res[str(i)] = np.around(counter[i]/number_instance*100.0, decimals=4)
+    # data = convert2numpy(dataset)
+    # 对应第一版的csv文件
+    # statics = Statistics(data[1])
+
+    data = convert2numpy_twoY(dataset)
+    # 对应第二版的csv文件,有pre和post两个y
+    statics_loop = []
+    statics_pre = Statistics(data[1])
+    statics_loop.append(statics_pre)
+
+    statics_post = Statistics(data[2])
+    statics_loop.append(statics_post)
+
+    idx = 0
+    for statics in statics_loop:
+        max_bug = statics.max_()
+        number_instance = statics.numberInstance()
+        unique, counts = np.unique(data[idx%2+1], return_counts=True)
+        counter = dict(zip(unique, counts))
+        print(counter)
+        res = dict()
+        if idx%2==0:
+            res['Release'] = filename+'-pre'
         else:
-            res[str(i)] = 0
-    if max_bug>8:
-        sum_=0
-        for j in range(9, max_bug+1):
-            if j in counter:
-                sum_+=counter[j]
-        res['gt 8'] = np.around(sum_/number_instance*100.0, decimals=4)
-    dataFrame = pd.DataFrame(res, index=[0])
-    with open('proportion.csv', 'a') as f:
-        dataFrame.to_csv(f, index=False, header=False)
+            res['Release'] = filename+'-post'
+
+        for i in range(max_bug + 1):
+            if i >= 9:
+                break
+            if i in counter:
+                if mode==1:
+                    res[str(i)] = np.around(counter[i] /
+                                        number_instance * 100.0, decimals=4)
+                else:
+                    res[str(i)] = counter[i]
+            else:
+                res[str(i)] = 0
+        if max_bug > 8:
+            sum_ = 0
+            for j in range(9, max_bug + 1):
+                if j in counter:
+                    sum_ += counter[j]
+            if mode==1:
+                res['gt 8'] = np.around(sum_ / number_instance * 100.0, decimals=4)
+            else:
+                res[str(i)] = sum_
+        dataFrame = pd.DataFrame(res, index=[0])
+        with open('proportion2.csv', 'a') as f:
+            dataFrame.to_csv(f, index=False, header=False)
+
+        idx += 1
 
     pass
+
 
 def proportion():
 
     # 先加入header
-    with open('proportion.csv', 'w') as f:
-    
+    with open('proportion2.csv', 'w') as f:
+
         columns = ['Release', 'Count 0', 'Count 1', 'Count 2',
                    'Count 3', 'Count 4', 'Count 5',
                    'Count 6', 'Count 7', 'Count 8',
@@ -188,25 +238,17 @@ def proportion():
         writer.writeheader()
     # 添加数据
     for dataset, filename in Processing().dataload():
-        proportion_(dataset, filename)
+        proportion_(dataset, filename, mode=1)
         pass
+
 
 def main():
 
     # 任务2, 计算比例
     proportion()
 
-
     # 任务1， 计算五个元素
     fiveElements()
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
